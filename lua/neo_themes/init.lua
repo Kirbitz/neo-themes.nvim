b3fc84a5cdf42818d215af8cd6e579152f9f6578
Kirbitz <tyler.marefke@gmail.com>
2023-06-08 16:40:55 -0500

    removed the test.lua file and started working on the ChangeTheme command

--- lua/neo_themes/init.lua
+++ lua/neo_themes/init.lua
@@ -5,5 +5,18 @@ M.setup = function(opts)
 	P(opts)
 end
 
-print("Loaded data test")
+vim.api.nvim_create_user_command("ChangeTheme", function(opts)
+	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. opts.fargs[1])
+	if not status_ok then
+		vim.notify("Failed to load colorscheme", vim.log.levels.ERROR, { title = "No Color Scheme Found" })
+		return
+	end
+end, {
+	nargs = 1,
+	complete = function()
+		return { "ron", "catppuccin-mocha" }
+	end,
+})
+
+print("Loaded data neo_themes")
 return M
