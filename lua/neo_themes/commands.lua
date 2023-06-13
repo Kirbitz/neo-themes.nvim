9091083e8e4943b0bd45d7ddc78c288690efe875
Kirbitz <tyler.marefke@gmail.com>
2023-06-13 14:29:52 -0500

    Created commands for random theme, next theme, and previous theme selections

--- lua/neo_themes/commands.lua
+++ lua/neo_themes/commands.lua
@@ -1,7 +1,9 @@
 local utils = require("neo_themes.utils")
 local completion = require("neo_themes.completion")
 
-vim.api.nvim_create_user_command("ChangeTheme", function(opts)
+local create_command = vim.api.nvim_create_user_command
+
+create_command("ChangeTheme", function(opts)
 	local theme = opts.fargs[1]
 	utils.setColorScheme(theme)
 end, {
@@ -12,7 +14,7 @@ end, {
 	end,
 })
 
-vim.api.nvim_create_user_command("SetTheme", function(opts)
+create_command("SetTheme", function(opts)
 	local theme = opts.fargs[1]
 	if utils.setColorScheme(theme) then
 		local path = utils.pathJoin(vim.fn.stdpath("cache"), "neo_themes", "theme_pref")
@@ -31,4 +33,41 @@ end, {
 	complete = function()
 		return completion.options
 	end,
+})
+
+create_command("RandomTheme", function()
+	math.randomseed(os.time())
+	local index = math.random(1, completion.size)
+	if index == completion.currentThemeIndex then
+		index = (index % completion.size) + 1
+	end
+	local theme = completion.options[index]
+	print(theme)
+	utils.setColorScheme(theme)
+end, {
+	desc = "Chooses a random theme to switch to that is different than the current theme",
+	nargs = 0,
+})
+
+create_command("NextTheme", function()
+	local index = (completion.currentThemeIndex % completion.size) + 1
+	local theme = completion.options[index]
+	print(theme)
+	utils.setColorScheme(theme)
+end, {
+	desc = "Sets the next theme in the completion list to be the current session theme",
+	nargs = 0,
+})
+
+create_command("PrevTheme", function()
+	local index = completion.currentThemeIndex - 1
+	if index == 0 then
+		index = completion.size
+	end
+	local theme = completion.options[index]
+	print(theme)
+	utils.setColorScheme(theme)
+end, {
+	desc = "Sets the previous theme in the completion list to be the current session theme",
+	nargs = 0,
 })
