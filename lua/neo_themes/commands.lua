2dcdd856d1bd0467aa1bfb3f83c00e9c2f6bf015
Kirbitz <tyler.marefke@gmail.com>
2023-06-12 19:19:39 -0500

    added function for set a persistant theme and updated how change theme works slightly

--- lua/neo_themes/commands.lua
+++ lua/neo_themes/commands.lua
@@ -1,11 +1,31 @@
+local utils = require("neo_themes.utils")
+
 vim.api.nvim_create_user_command("ChangeTheme", function(opts)
-	local status_ok, _ = pcall(vim.cmd, "colorscheme " .. opts.fargs[1])
-	if not status_ok then
-		vim.notify("Failed to load colorscheme", vim.log.levels.ERROR, { title = "No Color Scheme Found" })
-		return
-	end
+	local theme = opts.fargs[1]
+	utils.setColorScheme(theme)
 end, {
 	desc = "Changes the current colorscheme for the current session",
+	nargs = 1,
+	complete = function()
+		return vim.fn.getcompletion("", "color")
+	end,
+})
+
+vim.api.nvim_create_user_command("SetTheme", function(opts)
+	local theme = opts.fargs[1]
+	if utils.setColorScheme(theme) then
+		local path = utils.path_join(vim.fn.stdpath("cache"), "neo_themes", "theme_pref")
+
+		local file = io.open(path, "w+")
+		if not file then
+			vim.notify("Failed to open file", vim.log.levels.ERROR, { title = "IO Write Failure" })
+			return
+		end
+		file:write(theme)
+		file:close()
+	end
+end, {
+	desc = "Sets prefered the current colorscheme for current and future session",
 	nargs = 1,
 	complete = function()
 		return vim.fn.getcompletion("", "color")
